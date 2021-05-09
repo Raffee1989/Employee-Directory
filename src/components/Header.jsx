a3b83e05e386ed7036341a702bad3c35a318f9f5
Raffee <Raffee1989@gmail.com>
2021-05-09 03:47:30 -0700

    codes are added in Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Employee Directory</h1>
+
+      <h6>
+        Use search box to narrow or click on headings to filter your results
+      </h6>
+    </header>
+  );
+};
+
+export default Header;
