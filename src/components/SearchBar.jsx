c937e7ba4784d3a995138b65760ab01bb1a9ec7f
Raffee <Raffee1989@gmail.com>
2021-05-09 04:41:51 -0700

    searchbar is added from bootstrap and time to css it

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,2 +1,14 @@
 import React from "react";
 import "../styles/SearchBar.css"
+
+const SearchBar = () => {
+    return (
+        <div>
+            <form className="d-flex">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            </form>
+        </div>
+    );
+};
+
+export default SearchBar;
